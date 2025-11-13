31c47b36d31957bd4e5369d6819b595d45923070
NguyenSang <lenovo-x1@hs-nguyen.localdomain>
2025-11-13 15:52:16 +0700

    config(file):Update stack name

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Welcome to AWS Vietnam",
+            "message": "Welcome to AWS Vietnam User Group",
             # "location": ip.text.replace("\n", "")
         }),
     }
